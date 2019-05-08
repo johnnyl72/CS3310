936a580d4f1ce8e81b98c4a6e8d01a7d005ffa6a
jomamma417 <35053246+jomamma417@users.noreply.github.com>
2019-05-07 17:28:35 -0700

    updated

--- PROJECT2/Item.java
+++ PROJECT2/Item.java
@@ -4,16 +4,10 @@ public class Item implements Comparable<Item>{
 	private double weight;
 	private double profit;
 	private double ratio;
-	private int itemID;
 	
-	public Item(int itemID, double weight, double profit) {
-		this.itemID = itemID;
+	public Item(double profit, double weight) {
 		this.weight = weight;
 		this.profit = profit;
-	}
-	
-	public int getitemID() {
-		return itemID;
 	}
 	public double getWeight() {
 		return weight;
@@ -22,7 +16,7 @@ public class Item implements Comparable<Item>{
 		return profit;
 	}
 	public double getRatio() {
-		this.ratio = Math.round( (profit/weight) * 100.0 ) / 100.0; 
+		ratio = (Math.round( (profit/weight) * 100.0 ) / 100.0);
 		return ratio;
 	}
 	
@@ -31,7 +25,7 @@ public class Item implements Comparable<Item>{
 		return myRatio.compareTo(param.getRatio());
 	}
 	public String toString() {
-		return "Item ID: " + itemID + " Weight: " + weight + " Profit: " + profit + " Ratio: " + getRatio();
+		return "Weight: " + weight + " Profit: " + profit + " Ratio: " + getRatio();
 	}
 
 }
