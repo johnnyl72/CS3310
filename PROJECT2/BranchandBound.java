9be948bb4c25106eb4ca498958534878f1ef5631
jomamma417 <35053246+jomamma417@users.noreply.github.com>
2019-05-08 23:43:48 -0700

    stuck infinite loop

--- PROJECT2/BranchandBound.java
+++ PROJECT2/BranchandBound.java
@@ -7,31 +7,32 @@ import java.util.*;
  * nodes were checked.
  */
 
-public class BackTrack {
+public class BranchandBound {
 
-	public static double maxProfit;						// Greatest profit
-	public static String[] bestSet;						// Best Path
-	public static double W;								// Max capacity of the bag
-	public static ArrayList<Item> items; 				// Stored p & w array into this one class,
-														// Contains method to sort nonincreasing by p/w ratio
-	public static String[] include;
 	public static int nodesChecked = 0;
-	public static int numBest = -1;
+	public static double W;								// Max weight of bag
+	public static double maxProfit;					// Greatest profit
+	
+	public static ArrayList<Node> items;
+	
+	public static int counter = 0;
+	
+	
+
 	public static void main(String[] args) {
 		
 		Scanner kb = new Scanner(System.in);
 		
 		//INTIALIZE ARRAYLIST
-		System.out.println("How many items there are to potentially take: ");
-		int totalTake = kb.nextInt();
-		items = new ArrayList<Item>(totalTake);
-		
-		bestSet = new String[totalTake];
-		include = new String[totalTake];
+		//System.out.println("How many items there are to potentially take: ");
+		//int totalTake = kb.nextInt();
+		items = new ArrayList<Node>(5);
 		
 		//PROMPT MAX WEIGHT OF THE BAG
-		System.out.println("What is the max weight of the bag: ");
-		W = kb.nextInt();
+		//System.out.println("What is the max weight of the bag: ");
+		//W = kb.nextInt();
+		W = 16;
+		/*
 		//ADD ITEMS TO ARRAYLIST
 		for(int i = 1; i <= totalTake; i++) {
 			System.out.println("What is the profit of item " + (i));
@@ -40,82 +41,111 @@ public class BackTrack {
 			int weight = kb.nextInt();
 			items.add(new Item(profit, weight));
 		}
+		*/
+		items.add(new Node(0, 0));
+		items.add(new Node(40, 2));
+		items.add(new Node(30, 5));
+		items.add(new Node(50, 10));
+		items.add(new Node(10, 5));
+	
 		
-		//DISPLAY ARRAYLIST (Sort by ratio in nonincreasing order)
-		Collections.sort(items);	// Low to High
-		Collections.reverse(items); // High to Low
+		//Collections.sort(items);
+		
+		//DISPLAY ARRAYLIST (Sort by ratio in nonincreasing order)	
 		System.out.println("-----------------Current Items-----------------");
-		for(Item item: items) {
+		for(Node item: items) {
 			System.out.println(item.toString());
 		}
 		
-		// Begin algorithm
-		knapsack(0, items.get(0).getProfit(), items.get(0).getWeight());
 		
-		// Print out max profit and which items are taken & not, how many nodes were checked
-		System.out.println("Max Profit: " + maxProfit);
-		System.out.println("Nodes checked: " + nodesChecked);
+	
+		knapsack3(0, items.get(0).getProfit(), items.get(0).getWeight());
 		
 		kb.close();
-	}//end main
+	}// end main
+	public static void knapsack3(int n, double p, double w) {
+
+		PriorityQueue<Node> PQ = new PriorityQueue<Node>(); //Initialize
+		PQ.clear();
+		Node u = new Node();
+		Node v = new Node();
+		
+		v.level = 0;			//Initialize v to the root
+		v.profit = 0.0;			//Initialize v to the root
+		v.weight = 0.0;			//Initialize v to the root
+		double maxprofit = 0.0;
+		v.bound = bound(v);		//Compute v bound
+		
+		System.out.println("bound: " + v.bound);
+		System.out.println("profit: " + v.profit);
+		System.out.println("weight: " + v.weight);
+		System.out.println("--");
+		PQ.add(v);				//Insert root to PQ
+		
+		while(!PQ.isEmpty()) { 
+			PQ.poll();	//deque
+			
+			if(v.bound > maxprofit) {
+				//Remove node from front priority and assign it to v, visit left child
+				u.level = v.level + 1;
+				u.weight = v.weight + items.get(u.level).weight;
+				u.profit = v.profit + items.get(u.level).profit;
+				u.bound = bound(u);
+				System.out.println("bound: " + u.bound);
+				System.out.println("profit: " + u.profit);
+				System.out.println("weight: " + u.weight);
+				System.out.println("--");
+				if(u.weight <= W && u.profit > maxprofit) {
+					maxprofit = u.profit;
+				}
+				// Insert u to PQ
+				if(u.bound > maxprofit) {
+					PQ.add(u);
+				}
+				//Visit right child
+				u.level = v.level +1;
+				u.weight = v.weight;
+				u.profit = v.profit;
+				u.bound = bound(u);
+				System.out.println("bound: " + u.bound);
+				System.out.println("profit: " + u.profit);
+				System.out.println("weight: " + u.weight);
+				System.out.println("--");
 	
-	public static void knapsack(int i, double profit, double weight) {
-		// If profit > maxProfit and weight <= W, we update maxProfit and bestSet. 
-		if( (weight <= W) && (profit > maxProfit)) {
-			
-		// this set is best so far
-		maxProfit = profit;
-		if(numBest < i) {
-		numBest = i;
-		System.out.println("Take Node: " + (numBest));
+				if(u.bound > maxprofit)
+					PQ.add(u);
+				
+			}
 		}
-		else {
-		System.out.println("Not node: " + (i));
-		}
-		
-		bestSet = include;
-		
-		}
-		
-		if(promising(i, profit, weight)) {
-			include[i+1] = "Yes";
-			knapsack(i+1,(profit+items.get(i+1).getProfit()),(weight+items.get(i+1).getWeight()));
-			include[i+1] = "No";
-			knapsack(i+1,profit,weight);
-		}
-		
+			// ONLY DOES FIRST LEVEL
+
 	}
-	public static boolean promising(int i, double profit, double weight) {
-		//A check for promising
-		++nodesChecked;
-		int j; 
-		int k; 
-		double totweight;
-		double bound;
+	
+	public static float bound(Node u) {
+		int j;
+		int k;
+		double weight;
+		float bound;
 		
-		//A check for backtrack here
-		if(weight >= W) {
-			++nodesChecked;
-			return false;
-		}
-		else
-			
-			j = i + 1;
-			bound = profit;
-			totweight = weight;
-			//Calculate totweight and part of the bound needed for the next part
-			while((j <= (items.size()-1)) && (totweight + items.get(j).getWeight() <= W)) {
-				totweight = (totweight + items.get(j).getWeight());
-				bound = bound + items.get(j).getProfit();
+		if(u.weight >= W)
+			return 0;
+		else {
+			bound = (float) u.profit;
+			j = u.level + 1;
+			weight = u.weight;
+			while( (j <= (items.size() -1)) && (weight + items.get(j).weight <= W)) {
+				weight = weight + items.get(j).weight;
+				bound = (float) (bound + items.get(j).profit);
 				j++;
 			}
-			//Calculate true bound
-			k = j;
-			if (k <= (items.size()-1)) {
-					bound = bound + (W-totweight) * (items.get(k).getRatio());;
-			}
-		// A.K.A. if bound <= maxProfit, we can backtrack	
-		return bound > maxProfit;
-	}
+		}
+		k = j;
+		if( k <= (items.size()-1)) {
+			bound = (float) (bound + (W-weight) * (items.get(k).getRatio()));
+		}
+		return bound;
+			
+	}//end bound
+	
 	
 }
