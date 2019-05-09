280a8ec1c0becff1047ad533f8d477a26530b604
jomamma417 <35053246+jomamma417@users.noreply.github.com>
2019-05-08 21:39:01 -0700

    Update BranchandBound.java

--- PROJECT2/BranchandBound.java
+++ PROJECT2/BranchandBound.java
@@ -1,6 +1,5 @@
 package Project2;
 import java.util.*;
-import java.util.PriorityQueue;
 
 /**
  * When each algorithm has found a solution, print out
@@ -11,84 +10,89 @@ import java.util.PriorityQueue;
 public class BranchandBound {
 
 	public static int nodesChecked = 0;
-	public static double W;						// Max weight of bag
-	public static double maxProfit = 0;			// Greatest profit
-	public static double[] p;						// Profit array
-	public static double[] w;						// Weight array
+	public static double W;								// Max weight of bag
+	public static double maxProfit;					// Greatest profit
 	
+	public static ArrayList<Node> items;
+	
+	public static int counter = 0;
 	public static void main(String[] args) {
-		PriorityQueue<Node> PQ = new PriorityQueue<>();
-		PQ.add(new Node(50, 10));
-		PQ.add(new Node(30, 5));
-		PQ.add(new Node(40, 2));
-		PQ.add(new Node(10, 5));
 		
-		p = new double[4];
-		w = new double[4];
+		Scanner kb = new Scanner(System.in);
 		
-		int i = 0;
-		while(!PQ.isEmpty()) {
-			Node node1 = PQ.poll();
+		//INTIALIZE ARRAYLIST
+		//System.out.println("How many items there are to potentially take: ");
+		//int totalTake = kb.nextInt();
+		items = new ArrayList<Node>(4);
 		
-			p[i] = node1.getProfit();
-			System.out.println("p " + (i+1) + " element " + p[i]);
-			w[i] = node1.getWeight();
-			System.out.println("w " + (i+1) + " element " + w[i]);
-			
-			System.out.println(node1.toString());
-			System.out.println("--------------------------");
-			
+		//PROMPT MAX WEIGHT OF THE BAG
+		//System.out.println("What is the max weight of the bag: ");
+		//W = kb.nextInt();
+		W = 16;
+		/*
+		//ADD ITEMS TO ARRAYLIST
+		for(int i = 1; i <= totalTake; i++) {
+			System.out.println("What is the profit of item " + (i));
+			int profit = kb.nextInt();
+			System.out.println("What is the weight of item " + (i));
+			int weight = kb.nextInt();
+			items.add(new Item(profit, weight));
+		}
+		*/
+		items.add(new Node(40, 2));
+		items.add(new Node(50, 10));
+		items.add(new Node(30, 5));
+		items.add(new Node(10, 5));
+		Collections.sort(items);
+		//DISPLAY ARRAYLIST (Sort by ratio in nonincreasing order)	
+		System.out.println("-----------------Current Items-----------------");
+		for(Node item: items) {
+			System.out.println(item.toString());
+		}
+		
+		knapsack3(0, items.get(0).getProfit(), items.get(0).getWeight(), 90);
+		
+		kb.close();
+	}// end main
+	public static void knapsack3(int n, double p, double w, double maxProfit1) {
 	
-			i++;
-		}
-	
-		System.out.println("P ARRAY CONTENTS: ");
-		for(Double pArray: p) {
-			System.out.print(pArray.toString() + " ");
-		}
-		System.out.println("\nW ARRAY CONTENTS: ");
-		for(Double wArray: w) {
-			System.out.print(wArray.toString() + " ");
-		}
-		
-		knapsack3(0, p, w, 16 );
-		System.out.println();
-		System.out.println(maxProfit);
-		
-	}
-	public static void knapsack3(int n, double[] p, double[] w, int W) {
-		PriorityQueue<Node> PQ = new PriorityQueue<>(); //Declare and initialize PQ
-		Node u = new Node(p[0],w[0]);
+		PriorityQueue<Node> PQ = new PriorityQueue<Node>(); //Initialize
+		Node u = new Node();
 		Node v = new Node();
 		
+		PQ.clear();
 		v.level = 0;			//Initialize v to the root
 		v.profit = 0;			//Initialize v to the root
 		v.weight = 0;			//Initialize v to the root
+		maxProfit1 = 0;
 		v.bound = bound(v);		//Compute v bound
 		PQ.add(v);				//Insert root to PQ
 		
 		while(!PQ.isEmpty())
 			PQ.remove(v);
 		
-		if(v.bound > maxProfit) {
-			//Remove node fromt front priority and assign it to v, visit left child
-			u.level = v.level + 1;
-			u.weight = v.weight + w[u.level];
-			u.profit = v.profit + p[u.level];
+		if(v.bound > maxProfit1) {
+			//Remove node from front priority and assign it to v, visit left child
+			u.level = v.getLevel() + 1;
+			u.weight = v.weight + items.get(u.level).weight;
+			u.profit = v.profit + items.get(u.level).profit;
 			u.bound = bound(u);
 			
-			if(u.weight <= W && u.profit > maxProfit)
-				maxProfit = u.profit;
+			if(u.weight <= W && u.profit > maxProfit1) {
+				maxProfit1 = u.profit;
+			}
 			// Insert u to PQ
-			if(u.bound > maxProfit)
+			if(u.bound > maxProfit1) {
 				PQ.add(u);
+			}
 			u.weight = v.weight;
 			u.profit = v.profit;
 			u.bound = bound(u);
 
-			if(u.bound > maxProfit)
-				PQ.add(u);
+			if(u.bound > maxProfit1)
+				PQ.add(u);	
 		}
+		System.out.println(maxProfit1);
 	}
 	
 	public static double bound(Node u) {
@@ -100,19 +104,19 @@ public class BranchandBound {
 		if(u.weight >= W)
 			return 0;
 		else {
-			result = (float) u.profit;
+			result = u.profit;
 			j = u.level + 1;
 			totweight = u.weight;
-			while( j <= (w.length) && totweight + w[j] <= W) {
-				totweight = totweight +w[j];
-				result = result + p[j];
+			while( (j <= (items.size() -1)) && (totweight + items.get(j).weight <= W)) {
+				totweight = totweight + items.get(j).weight;
+				result = result + items.get(j).profit;
 				j++;
 			}
 		}
 		k = j;
-		if( k <= (w.length))
-			result = (result + (W- totweight) * p[k] / w[k]);
-		
+		if( k <= (items.size()-1)) {
+			result = result + (W-totweight) * (items.get(k).getRatio());
+		}
 		return result;
 			
 	}//end bound
