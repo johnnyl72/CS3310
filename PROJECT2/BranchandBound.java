e21d1bea19e511901b17b944b43ccff1212ca287
jomamma417 <35053246+jomamma417@users.noreply.github.com>
2019-05-09 00:29:55 -0700

    Update BranchandBound.java

--- PROJECT2/BranchandBound.java
+++ PROJECT2/BranchandBound.java
@@ -14,106 +14,85 @@ public class BranchandBound {
 	public static double maxProfit;					// Greatest profit
 	
 	public static ArrayList<Node> items;
-	
 	public static int counter = 0;
-	
-	
+	public static double maxprofit;
 
 	public static void main(String[] args) {
 		
 		Scanner kb = new Scanner(System.in);
 		
-		//INTIALIZE ARRAYLIST
-		//System.out.println("How many items there are to potentially take: ");
-		//int totalTake = kb.nextInt();
-		items = new ArrayList<Node>(5);
-		
-		//PROMPT MAX WEIGHT OF THE BAG
-		//System.out.println("What is the max weight of the bag: ");
-		//W = kb.nextInt();
+		items = new ArrayList<Node>(5);	
 		W = 16;
-		/*
-		//ADD ITEMS TO ARRAYLIST
-		for(int i = 1; i <= totalTake; i++) {
-			System.out.println("What is the profit of item " + (i));
-			int profit = kb.nextInt();
-			System.out.println("What is the weight of item " + (i));
-			int weight = kb.nextInt();
-			items.add(new Item(profit, weight));
-		}
-		*/
 		items.add(new Node(0, 0));
 		items.add(new Node(40, 2));
 		items.add(new Node(30, 5));
 		items.add(new Node(50, 10));
 		items.add(new Node(10, 5));
 	
-		
-		//Collections.sort(items);
-		
 		//DISPLAY ARRAYLIST (Sort by ratio in nonincreasing order)	
 		System.out.println("-----------------Current Items-----------------");
 		for(Node item: items) {
 			System.out.println(item.toString());
 		}
-		
-		
 	
 		knapsack3(0, items.get(0).getProfit(), items.get(0).getWeight());
-		
-		kb.close();
+
 	}// end main
 	public static void knapsack3(int n, double p, double w) {
 
 		PriorityQueue<Node> PQ = new PriorityQueue<Node>(); //Initialize
 		PQ.clear();
+
 		Node u = new Node();
 		Node v = new Node();
 		
-		v.level = 0;			//Initialize v to the root
-		v.profit = 0.0;			//Initialize v to the root
-		v.weight = 0.0;			//Initialize v to the root
-		double maxprofit = 0.0;
-		v.bound = bound(v);		//Compute v bound
+		v.setLevel(0);		//Initialize v to the root
+		v.setProfit(0.0);			//Initialize v to the root
+		v.setWeight(0.0);			//Initialize v to the root
+		maxprofit = 0.0;
+		v.setBound(bound(v));		//Compute v bound
 		
-		System.out.println("bound: " + v.bound);
-		System.out.println("profit: " + v.profit);
-		System.out.println("weight: " + v.weight);
+		System.out.println("bound: " + v.getBound());
+		System.out.println("profit: " + v.getProfit());
+		System.out.println("weight: " + v.getWeight());
 		System.out.println("--");
-		PQ.add(v);				//Insert root to PQ
-		
+		PQ.add(v);				//Insert root to PQ	
 		while(!PQ.isEmpty()) { 
-			PQ.poll();	//deque
+			PQ.remove();	//deque
 			
-			if(v.bound > maxprofit) {
+			if(v.getBound() > maxprofit) {
 				//Remove node from front priority and assign it to v, visit left child
-				u.level = v.level + 1;
-				u.weight = v.weight + items.get(u.level).weight;
-				u.profit = v.profit + items.get(u.level).profit;
-				u.bound = bound(u);
-				System.out.println("bound: " + u.bound);
-				System.out.println("profit: " + u.profit);
-				System.out.println("weight: " + u.weight);
+				u.setLevel(v.getLevel() + 1);
+				u.setProfit(v.getProfit() + items.get(u.getLevel()).getProfit());
+				u.setWeight(v.getWeight() + items.get(u.getLevel()).getWeight());
+				u.setBound(bound(u));
+				System.out.println("bound: " + u.getBound());
+				System.out.println("profit: " + u.getProfit());
+				System.out.println("weight: " + u.getWeight());
 				System.out.println("--");
-				if(u.weight <= W && u.profit > maxprofit) {
-					maxprofit = u.profit;
+				if(u.getWeight() <= W && u.getProfit() > maxprofit) {
+					maxprofit = u.getProfit();	//40
 				}
-				// Insert u to PQ
-				if(u.bound > maxprofit) {
+				// Insert u(1,1) to PQ
+				if(u.getBound() > maxprofit) {
 					PQ.add(u);
 				}
-				//Visit right child
-				u.level = v.level +1;
-				u.weight = v.weight;
-				u.profit = v.profit;
-				u.bound = bound(u);
-				System.out.println("bound: " + u.bound);
-				System.out.println("profit: " + u.profit);
-				System.out.println("weight: " + u.weight);
+				
+				//Visit unvisited right child
+				u.setLevel(v.getLevel()+1);;
+				u.setWeight(v.getWeight());
+				u.setProfit(v.getProfit());
+				u.setBound(bound(u));
+				System.out.println("bound: " + u.getBound());
+				System.out.println("profit: " + u.getProfit());
+				System.out.println("weight: " + u.getWeight());
 				System.out.println("--");
-	
-				if(u.bound > maxprofit)
+				//Is bound promising?
+				if(u.getBound() > maxprofit) {
 					PQ.add(u);
+					System.out.println(PQ.toString() + " |");
+					
+				}
 				
 			}
 		}
@@ -127,20 +106,20 @@ public class BranchandBound {
 		double weight;
 		float bound;
 		
-		if(u.weight >= W)
+		if(u.getWeight() >= W)
 			return 0;
 		else {
-			bound = (float) u.profit;
-			j = u.level + 1;
-			weight = u.weight;
-			while( (j <= (items.size() -1)) && (weight + items.get(j).weight <= W)) {
-				weight = weight + items.get(j).weight;
-				bound = (float) (bound + items.get(j).profit);
+			bound = (float) u.getProfit();
+			j = u.getLevel() + 1;
+			weight = u.getWeight();
+			while( (j < (items.size())) && (weight + items.get(j).getWeight() <= W)) {
+				weight = weight + items.get(j).getWeight();
+				bound = (float) (bound + items.get(j).getProfit());
 				j++;
 			}
 		}
 		k = j;
-		if( k <= (items.size()-1)) {
+		if( k < (items.size())) {
 			bound = (float) (bound + (W-weight) * (items.get(k).getRatio()));
 		}
 		return bound;
